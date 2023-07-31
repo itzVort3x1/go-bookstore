7dd70399024850706436506c406b51d975107fca
itzVort3x1 <saikaustubh10@gmail.com>
2023-07-31 08:23:16 +0530

    inital commit

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import "github.com/gorilla/mux"
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.updateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.deleteBook).Methods("DELETE")
+}
